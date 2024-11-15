90d8791dcd7e74d9272150189c0d558924cdee7e
alisedarta <a2000s@inbox.lv>
2024-11-15 14:50:32 +0100

    feat: add filters FP-2834

--- src/components/Overview.tsx
+++ src/components/Overview.tsx
@@ -1,11 +1,47 @@
+import { useEffect, useState } from "react";
 import useFetchArtworks from "../hooks/useFetchArtworks";
 import GalleryGrid from "./GalleryGrid";
 
 function Overview() {
-  const artworks = useFetchArtworks();
+  const [isOnView, setIsOnView] = useState(
+    () => localStorage.getItem("isOnView") === "true"
+  );
+  const [isPublicDomain, setIsPublicDomain] = useState(
+    localStorage.getItem("isPublicDomain") === "true"
+  );
+  const [isHiddenGem, setIsHiddenGem] = useState(
+    () => localStorage.getItem("isHiddenGem") === "true"
+  );
+  const artworks = useFetchArtworks(isOnView, isPublicDomain, isHiddenGem);
+
+  useEffect(() => {
+    localStorage.setItem("isOnView", JSON.stringify(isOnView));
+    localStorage.setItem("isPublicDomain", JSON.stringify(isPublicDomain));
+    localStorage.setItem("isHiddenGem", JSON.stringify(isHiddenGem));
+  }, [isOnView, isPublicDomain, isHiddenGem]);
   return (
     <div className="container">
       <h1>Overview</h1>
+      <div className="filters-container">
+        <button
+          onClick={() => setIsHiddenGem(!isHiddenGem)}
+          className={isHiddenGem ? "" : "disabled-filter"}
+        >
+          Hidden gem
+        </button>
+        <button
+          onClick={() => setIsOnView(!isOnView)}
+          className={isOnView ? "" : "disabled-filter"}
+        >
+          On view
+        </button>
+        <button
+          onClick={() => setIsPublicDomain(!isPublicDomain)}
+          className={isPublicDomain ? "" : "disabled-filter"}
+        >
+          Public domain
+        </button>
+      </div>
       <GalleryGrid artworks={artworks} />
     </div>
   );
--- src/fetchApi.ts
+++ src/fetchApi.ts
@@ -1,9 +1,43 @@
-export const fetchArtworks = async () => {
+export const fetchArtworks = async (
+  isOnView: boolean,
+  isPublicDomain: boolean,
+  isHiddenGem: boolean
+) => {
   try {
-    const response = await fetch(
-      "https://api.artic.edu/api/v1/artworks?page=1&limit=10&fields=title,artist_title,place_of_origin,date_display,image_id"
-    );
+    const query = {
+      query: {
+        bool: {
+          must: [
+            ...(isOnView ? [{ term: { is_on_view: true } }] : []),
+            ...(isPublicDomain ? [{ term: { is_public_domain: true } }] : []),
+            ...(isHiddenGem
+              ? [{ term: { has_not_been_viewed_much: true } }]
+              : []),
+          ],
+        },
+      },
 
+      limit: 10,
+
+      fields: [
+        "title",
+        "artist_title",
+        "place_of_origin",
+        "date_display",
+        "image_id",
+      ],
+    };
+
+    const response = await fetch(
+      "https://api.artic.edu/api/v1/artworks/search",
+      {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(query),
+      }
+    );
     if (!response.ok) {
       throw new Error(`Error fetching API. Status: ${response.status}`);
     }
